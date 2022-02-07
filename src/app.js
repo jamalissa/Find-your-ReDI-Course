4cd817d5b8dfffb8f341d130c3b13f7aebee41ed
marcosfsousa <marcosfmsousa@gmail.com>
2022-02-07 20:25:06 +0100

    added JSON and JS logic

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import React from "react";
 import Header from "./header";
+import Quiz from "./quiz";
 
 export default function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Quiz />
+    </div>
+  );
 }
---
+++ src/quiz.js
@@ -0,0 +1,22 @@
+import React from "react";
+import data from "./assets/data.json";
+
+export default function Quiz() {
+  return <div className="container"></div>;
+}
+
+// display Answers in buttons //
+
+let answerDisplay = "";
+
+for (let i = 0; i <= 3; i++) {
+  answerDisplay += data["3"]["answer"][i] + " ";
+}
+
+console.log(answerDisplay);
+
+// increment the final score //
+
+// listen event handler for each button click
+// that increments a specific variable
+// maybe a function with
