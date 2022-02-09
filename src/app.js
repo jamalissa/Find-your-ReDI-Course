4ca3a0030a97c7203bb965978ac6d972a3802341
marcosfsousa <marcosfmsousa@gmail.com>
2022-02-09 19:57:10 +0100

    Created some styling for this component

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Header from "./header";
 import Quiz from "./quiz";
 
 export default function App() {
   return (
     <div>
-      <Header />
       <Quiz />
     </div>
   );
--- src/quiz.js
+++ src/quiz.js
@@ -1,8 +1,21 @@
 import React from "react";
 import data from "./assets/data.json";
+import "./quiz.css";
 
 export default function Quiz() {
-  return <div className="container"></div>;
+  return (
+    <div className="wrapper-quiz">
+      <div className="question-box">
+        <h3>"Where is the dark side of the moon ?"</h3>
+      </div>
+      <div className="answer-box">
+        <button className="btn-answer">1</button>
+        <button className="btn-answer">2</button>
+        <button className="btn-answer">3</button>
+        <button className="btn-answer">4</button>
+      </div>
+    </div>
+  );
 }
 
 // display Answers in buttons //
