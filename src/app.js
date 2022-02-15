4444fadcbf42b84cd54f16fd52a00fb81fc4d699
marcosfsousa <marcosfmsousa@gmail.com>
2022-02-15 18:12:23 +0100

    Improved logic and styling of App

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Quiz from "./quiz";
+import data from "./assets/data.json";
 
 export default function App() {
   return (
     <div>
-      <Quiz />
+      <Quiz question={data[1]["question"]} answer={data[1]["answer"]} />
+      <Quiz question={data[2]["question"]} answer={data[2]["answer"]} />
+      <Quiz question={data[3]["question"]} answer={data[3]["answer"]} />
     </div>
   );
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,32 +1,38 @@
 import React from "react";
-import data from "./assets/data.json";
 import "./quiz.css";
 
-export default function Quiz() {
+export default function Quiz(props) {
+  // Listen to btn-clicks (not working because of confliting class name)
+
+  // document.querySelector(".btn-answer").addEventListener("click", function () {
+  //   let result;
+  //   result += document.querySelector(".btn-answer").value;
+  //   console.log(result);
+  // });
+
   return (
     <div className="wrapper-quiz">
       <div className="question-box">
-        <h3>"Where is the dark side of the moon ?"</h3>
+        <h3>{props.question}</h3>
       </div>
       <div className="answer-box">
-        <button className="btn-answer">1</button>
-        <button className="btn-answer">2</button>
-        <button className="btn-answer">3</button>
-        <button className="btn-answer">4</button>
+        <button className="btn-answer" value="a">
+          {props.answer[0]}
+        </button>
+        <button className="btn-answer" value="b">
+          {props.answer[1]}
+        </button>
+        <button className="btn-answer" value="c">
+          {props.answer[2]}
+        </button>
+        <button className="btn-answer" value="d">
+          {props.answer[3]}
+        </button>
+        <button className="btn-next">Next Question</button>
       </div>
     </div>
   );
 }
-
-// display Answers in buttons //
-
-let answerDisplay = "";
-
-for (let i = 0; i <= 3; i++) {
-  answerDisplay += data["3"]["answer"][i] + " ";
-}
-
-console.log(answerDisplay);
 
 // increment the final score //
 
