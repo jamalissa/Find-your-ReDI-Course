88f7ff5d3e0a3f193d9b45d22b017ed34388b676
marcosfsousa <marcosfmsousa@gmail.com>
2022-03-01 16:32:37 +0100

    Improved functionality

--- src/result.js
+++ src/result.js
@@ -1,16 +1,13 @@
 import React from "react";
 import { getMostFrequent } from "./app";
 import "./result.css";
-// import webDev from "./assets/web_dev_track.png";
-// import userCenter from "./assets/user_center_track.png";
-// import dataAnalyst from "./assets/data_analyst_track.png";
-// import cloudInfra from "./assets/cloud_infra_track.png";
-
-// const resultImg = document.getElementById("result-img");
-let resultInfoPara = document.querySelector(".result-info-para");
+import webDev from "./assets/web_dev_track.png";
+import userCenter from "./assets/user_center_track.png";
+import dataAnalyst from "./assets/data_analyst_track.png";
+import cloudInfra from "./assets/cloud_infra_track.png";
 
 export default function Result({ answState }) {
-  // console.log(getMostFrequent(answState));
+  console.log(getMostFrequent(answState));
   // calling most frequent function to then display results
 
   function restartQuiz() {
@@ -21,20 +18,36 @@ export default function Result({ answState }) {
   let quizResult = getMostFrequent(answState);
 
   function getImage() {
-    if (quizResult === 0) {
-      return console.log("this is zero");
-      // resultInfoPara.textContent = "Web Dev Development!";
-    } else if (quizResult === 1) {
-      return console.log("this is two");
-      // resultInfoPara.textContent = "User Centered Design";
-    } else if (quizResult === 2) {
-      return (resultInfoPara.textContent = "Data Analyst");
-    } else if (quizResult === 3) {
-      return (resultInfoPara.textContent = "Cloud Infrastructure");
+    if (quizResult === "0") {
+      return (
+        <img src={webDev} className="result-info-img" alt="web_dev_image"></img>
+      );
+    } else if (quizResult === "1") {
+      return (
+        <img
+          src={userCenter}
+          className="result-info-img"
+          alt="web_dev_image"
+        ></img>
+      );
+    } else if (quizResult === "2") {
+      return (
+        <img
+          src={dataAnalyst}
+          className="result-info-img"
+          alt="web_dev_image"
+        ></img>
+      );
+    } else if (quizResult === "3") {
+      return (
+        <img
+          src={cloudInfra}
+          className="result-info-img"
+          alt="web_dev_image"
+        ></img>
+      );
     }
   }
-  console.log(getImage());
-  // console.log(resultImg.src);
 
   return (
     <div>
@@ -43,8 +56,11 @@ export default function Result({ answState }) {
           <h3 className="result-heading">The results are in!</h3>
         </div>
         <div className="result-info">
-          <p className="result-info-para"></p>
-          {/* <img src={webDev} alt="" id="result-img"></img> */}
+          <p className="result-info-para">
+            Based on your choices it looks like you'd be best suited for this
+            course <br /> &#8659;
+          </p>
+          {getImage()}
           <button className="btn-restart" onClick={() => restartQuiz()}>
             Back to the start
           </button>
